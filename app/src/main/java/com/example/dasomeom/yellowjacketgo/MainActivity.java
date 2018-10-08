5b11894ea3e84b068ed3177d44d8f3b01eb6a1ba
Dasom Eom <deom119@gmail.com>
2018-10-07 23:54:41 -0400

    update

--- app/src/main/java/com/example/dasomeom/yellowjacketgo/MainActivity.java
+++ app/src/main/java/com/example/dasomeom/yellowjacketgo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.dasomeom.yellowjacketgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    private Button bLogin;
+    private Button bRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bLogin = (Button) findViewById(R.id.mLogin);
+        bRegister = (Button) findViewById(R.id.mRegister);
+        bLogin.setOnClickListener(this);
+        bRegister.setOnClickListener(this);
+
+
+
+    }
+
+    public void onClick(View v) {
+        if (v == bLogin) {
+            Intent intLog = new Intent (this, login.class);
+            startActivity(intLog);
+        }
+
+        if (v == bRegister) {
+            Intent intent2 = new Intent (this, register.class);
+            startActivity(intent2);
+        }
     }
 }
